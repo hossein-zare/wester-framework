303849a3482d767d27e221dc4431ad399e3741e8
unknown <zarehossein.hz1997@gmail.com>
2019-12-20 15:29:16 +0330

    Major changes

--- app/http/bridges/DataTrimmer.php
+++ app/http/bridges/DataTrimmer.php
@@ -3,8 +3,9 @@
 namespace App\Http\Bridges;
 
 use Powerhouse\Gate\Bridges\DataTrimmer as DataTrimmerBridge;
+use Contracts\Gate\BridgeInterface;
 
-class DataTrimmer extends DataTrimmerBridge
+class DataTrimmer extends DataTrimmerBridge implements BridgeInterface
 {
 
     // Methods are present in the powerhouse.
